98c6091bbf140ffd6f1150fc52ab7c175140aae3
XZ361 <2457307263@qq.com>
2024-02-14 09:19:28 +0800

    测试

---
+++ 脚手架/单文件组件/App.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+     <h3>{{ msg }}</h3>
+   </div>
+</template>
+<script>
+export default {
+   data() {
+     return {
+       msg:'单文件组件'
+     }
+   },
+};
+</script>
+<style scoped>
+h3{
+  color: red;
+}
+</style>
